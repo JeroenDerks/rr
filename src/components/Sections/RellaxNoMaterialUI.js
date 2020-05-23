dbf17b7e20c0f491fb0bf6f6001c981a91e06012
Jeroen Derks <jede@dhigroup.com>
2020-05-23 16:21:36 +0200

    stash

--- src/components/Sections/RellaxNoMaterialUI.js
+++ src/components/Sections/RellaxNoMaterialUI.js
@@ -1,60 +1,153 @@
 import React from 'react';
-import { RellaxWrapper } from 'react-rellax-wrapper';
-import { Box } from '@material-ui/core/';
+import _ from 'lodash';
+import DialogWindow from './Dialog';
+import PlusIcon from 'assets/images/plus-icon-stroked.svg';
 import { gridStyle } from 'styles/global';
+import { Parallax } from 'react-scroll-parallax';
+import { useController } from 'react-scroll-parallax';
 import { AppContext } from 'App';
 import 'styles/global.css';
 
-export default function RellaxNoMaterialUI({
-  firstColumn,
-  images,
-  title,
-  i,
-  textOffset,
-}) {
+import ImageFadeIn from 'react-image-fade-in';
+
+export default function RellaxNoMaterialUI({ images, textOffset }) {
   const {
-    state: { minHeight },
+    state: { columnWidth },
   } = React.useContext(AppContext);
 
-  const [imageArr, setImages] = React.useState(images);
-  const [rows, setRows] = React.useState(new Array(10).fill('null'));
-  const [columns, setColumns] = React.useState(new Array(10).fill(null));
+  const { parallaxController } = useController();
+  React.useLayoutEffect(() => {
+    const handler = () => parallaxController.update();
+    window.addEventListener('load', handler);
+    return () => window.removeEventListener('load', handler);
+  }, [parallaxController]);
 
-  console.log('nomateiral ui');
+  const [fullScreenImg, setFullScreenImg] = React.useState();
+  const [imageArr, setImages] = React.useState(images);
+  const [open, setOpen] = React.useState(false);
+  const style = gridStyle();
+
+  const setImg = (i) => {
+    let imgs = _.cloneDeep(imageArr);
+    imgs.forEach((img, _i) => {
+      i === _i ? (img.expanded = !img.expanded) : (img.expanded = false);
+    });
+    setImages([...imgs]);
+  };
+
+  const imageToFullscreen = (e, i) => {
+    // const { x, y, width, height } = e.target.parentNode.getBoundingClientRect();
+
+    // const distX = x - window.innerWidth * 0.5;
+    // const distY = y - window.innerHeight * 0.5;
+
+    // const offsetX = parseInt((distX / width) * 100) + '% ';
+    // const offsetY = parseInt((distY / height) * 100) + '% ';
+
+    // console.log(offsetX, offsetY);
+
+    // let imgs = _.cloneDeep(imageArr);
+    // imgs.forEach((img, _i) => {
+    //   if (i === _i) {
+    //     img.huge = true;
+    //     img.offset = offsetX + offsetY;
+    //   } else img.expanded = false;
+    // });
+    // setImages([...imgs]);
+    // setFullScreenImg({
+    //   image: imageArr[i].image,
+    //   offset: offsetX + offsetY + ' 1',
+    // });
+
+    setFullScreenImg(imageArr[i]);
+
+    e.stopPropagation();
+    setOpen(true);
+  };
+
+  const handleClose = (value) => {
+    setOpen(false);
+    setFullScreenImg();
+  };
+
+  console.log(imageArr);
+
   return (
-    <>
-      {rows.map((i) => (
-        <div
-          key={i}
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            height: '75vh',
-            width: '100%',
-            justifyContent: 'space-between',
-            border: '1px solid grey',
-            paddingTop: '15%',
-          }}
-          className={'wrapper' + i}
-        >
-          {columns &&
-            columns.map((j) => (
-              <RellaxWrapper
-                speed={(Math.random() - 0.5) * 10}
-                center={true}
-                key={j}
-              >
+    <div className={style.categoryWrapper}>
+      <DialogWindow
+        fullScreenImg={fullScreenImg && fullScreenImg.image}
+        handleClose={handleClose}
+        open={open}
+      />
+      {imageArr &&
+        imageArr.map(
+          ({ expanded, huge, image, margin, offset, width, y }, i) => (
+            <div
+              style={{
+                height: '100%',
+                position: 'absolute',
+                transition: 'z-index .5s',
+                zIndex:
+                  expanded === false ? 0 : expanded === undefined ? 100 : 50,
+                pointerEvents: 'none',
+                boxSizing: 'border-box',
+                margin: margin,
+                marginLeft: i * 7 + '%',
+              }}
+              key={i}
+            >
+              <Parallax y={y}>
                 <div
+                  onClick={() => setImg(i)}
                   style={{
-                    width: '100px',
-                    height: '400px',
-                    border: '1px solid coral',
+                    transformOrigin: huge === true ? offset : 'center',
+                    transform:
+                      huge === true
+                        ? 'scale3d(2,2,1)'
+                        : expanded !== true
+                        ? 'scale3d(0.3,0.3,1)'
+                        : 'scale3d(1,1,1)',
+                    width: width * columnWidth,
+                    pointerEvents: 'all',
                   }}
-                ></div>
-              </RellaxWrapper>
-            ))}
-        </div>
-      ))}
-    </>
+                  className={style.transition}
+                >
+                  <ImageFadeIn
+                    width={'100%'}
+                    height={'100%'}
+                    src={imaimagegeSrc}
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      boxShadow:
+                        width !== 6 && '5px 5px 10px 5px rgba(0,0,0,0.27)',
+                      cursor: 'pointer',
+                    }}
+                  />
+                  {/* <img
+                    src={image}
+                    alt={image}
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      boxShadow:
+                        width !== 6 && '5px 5px 10px 5px rgba(0,0,0,0.27)',
+                      cursor: 'pointer',
+                    }}
+                  /> */}
+                  {expanded === true && (
+                    <img
+                      src={PlusIcon}
+                      alt={'plus icon'}
+                      className={style.icon}
+                      onClick={(e) => imageToFullscreen(e, i)}
+                    />
+                  )}
+                </div>
+              </Parallax>
+            </div>
+          )
+        )}
+    </div>
   );
 }
